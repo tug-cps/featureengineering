3fb8adccba592180c25f5fbf446e60f1b9a6eed4
swilfling <sandra.wilfling@tugraz.at>
2022-07-26 15:29:15 +0200

    Revert "Added composite transformer"

--- compositetransformers/maskfeats/__init__.py
+++
@@ -1,4 +0,0 @@
-from .maskfeats import MaskFeats
-from .maskfeatsinplace import MaskFeats_Inplace
-from .maskfeatsaddition import MaskFeats_Addition
-from .maskfeatsexpanded import MaskFeats_Expanded
