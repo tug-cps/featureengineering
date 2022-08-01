1769030d8906f7926ef686078800be47f5248fb1
swilfling <sandra.wilfling@student.tugraz.at>
2022-08-01 12:24:37 +0200

    add sampleweight, tests

---
+++ tests/test_lpf.py
@@ -0,0 +1,42 @@
+from featureengineering.filters import ButterworthFilter, Filter
+
+
+def test_filter_params_baseclass():
+    filter_1 = ButterworthFilter(T=20, order=3)
+    params_but = filter_1.get_params(deep=True)
+    filter_basic = Filter()
+    params_basic = filter_basic.get_params(deep=True)
+    # Are params different from each other?
+    assert(params_but != params_basic)
+    # Are basic params in subclass params?
+    assert([param in params_but for param in params_basic])
+
+
+def test_store_load_filter():
+    path = "./test_output"
+    filename = "LPF.pickle"
+
+    filter_1 = ButterworthFilter(T=20, order=3)
+    filter_1.save_pkl(path, filename)
+    filter_2 = ButterworthFilter.load_pkl(path, filename)
+    assert(filter_1.get_params() == filter_2.get_params())
+    assert(filter_1.get_params(deep=True) == filter_2.get_params(deep=True))
+
+
+def test_store_load_filter_baseclass():
+    path = "./test_output"
+    filename = "LPF.pickle"
+
+    filter_1 = ButterworthFilter(T=20, order=3)
+    filter_1.save_pkl(path, filename)
+
+    filter_2 = ButterworthFilter.load_pkl(path, filename)
+    filter_3 = Filter.load_pkl(path, filename)
+    assert(filter_2.get_params() == filter_3.get_params())
+    assert (filter_2.get_params(deep=True) == filter_3.get_params(deep=True))
+
+
+if __name__ == "__main__":
+    test_filter_params_baseclass()
+    test_store_load_filter()
+    test_store_load_filter_baseclass()
