b9492f05f31a6b6c5f3aa9e9d8d9a1042a8dba87
swilfling <sandra.wilfling@student.tugraz.at>
2022-08-09 09:14:56 +0200

    new test

--- tests/test_statistical.py
+++ tests/test_statistical.py
@@ -12,6 +12,17 @@ def test_statfeats():
         for win in range(25):
             assert(np.all(X_tr[win*4:win*4+4,feat]==np.min(X[win*4:win*4+4,feat],axis=0)))
 
+
+def test_statfeats_rest():
+    X = np.random.randn(102,3)
+    statfeats = StatisticalFeaturesNumpy(window_size=4,statistical_features=['min'])
+    X_tr = statfeats.fit_transform(X)
+    assert(X_tr.shape==X.shape)
+    for feat in range(3):
+        for win in range(25):
+            assert(np.all(X_tr[win*4:win*4+4,feat]==np.min(X[win*4:win*4+4,feat],axis=0)))
+        assert (np.all(X_tr[100:102, feat] == np.min(X[100:102, feat], axis=0)))
+
 def test_statfeats_df():
     X = pd.DataFrame(data=np.random.randn(80,3), columns=['Feat_0','Feat_1','Feat_2'])
     statfeats = StatisticalFeaturesNumpy(window_size=4,statistical_features=['min','max'])
@@ -25,4 +36,5 @@ def test_statfeats_df():
 
 if __name__ == "__main__":
     test_statfeats()
+    test_statfeats_rest()
     test_statfeats_df()
