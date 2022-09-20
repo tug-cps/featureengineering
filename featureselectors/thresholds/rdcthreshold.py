520a99b5392dbde48fde37a99f865388ce5374d4
swilfling <sandra.wilfling@student.tugraz.at>
2022-09-20 13:32:08 +0200

    rdc threshold

--- featureselectors/thresholds/rdcthreshold.py
+++ featureselectors/thresholds/rdcthreshold.py
@@ -1,6 +1,6 @@
 import numpy as np
 from rdc import rdc
-
+import pandas as pd
 from . import FeatureSelectThreshold
 
 
@@ -12,11 +12,18 @@ class RDCThreshold(FeatureSelectThreshold):
     """
     def calc_coef(self, X, y=None, **fit_params):
         """
-        Calculate coefficients for feature selection trheshold.
+        Calculate coefficients for feature selection threshold.
         @param X: input features (n_samples x n_features)
         @param y: target features
         @return: coefficients
         """
+        if isinstance(X, pd.Series) or isinstance(X, pd.DataFrame):
+            X = X.to_numpy()
+        if isinstance(y, pd.Series) or isinstance(y, pd.DataFrame):
+            y = y.to_numpy()
+
         if X.shape[-1] > 0:
-            return rdc(X, y)
+            if y.ndim == 1:
+                y = y.reshape(y.shape[0], 1)
+            return np.array([rdc(X[...,i], y) for i in range(X.shape[-1])])
         return np.zeros(1)
